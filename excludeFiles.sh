9931fab286999ed9e6ea2dceb23c90a8725a5f54
Sidharth Saluja <67796130+SidharthSaluja@users.noreply.github.com>
2023-03-17 17:29:54 -0600

    Update excludeFiles.sh

--- excludeFiles.sh
+++ excludeFiles.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+##!/bin/bash
 
 # Find modified files that are not in the allowed folders
 modified_files=$(git status --porcelain | awk '{print $2}' | grep -vE "^about/|^posts/|^_freeze/")
 
-# Exclude each modified file from future commits
-for file in $modified_files
-do
-    if [ -f "$file" ]; then
-        git update-index --assume-unchanged $file
-    fi
-done
+if [ ! -z "$modified_files" ]; then
+    echo "Cannot commit files outside of 'about/', 'posts/', and '_freeze/' folders. Please move the modified files into one of these folders before committing."
+    echo "Please exclude the following files:"
+    echo "$modified_files"
+    echo "\n"
+    exit 1
+fi;
